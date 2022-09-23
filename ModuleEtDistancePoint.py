ae7e66f887ecf62af6f17dd8c4f0614ef72c9a4f
MortimerAlien <113467961+MortimerAlien@users.noreply.github.com>
2022-09-23 13:25:24 +0000

    Update and rename ModuleEtDistance3d.py to ModuleEtDistancePoint.py

--- ModuleEtDistancePoint.py
+++ ModuleEtDistancePoint.py
@@ -1,24 +1,35 @@
 #!/usr/bin/python3
 import numpy as np
 
-class Point:
+
+class AffichableMixin:
+    str_format = "Venus"
+    
+    def __str__(self):
+        return self.str_format.format(self=self)
+
+
+class NomAutomatiqueMixin:
+    ordinal = 65
+    
+    def __init__(self):
+        self.lettre = chr(NomAutomatiqueMixin.ordinal)
+        NomAutomatiqueMixin.ordinal += 1
+
+
+class Point(AffichableMixin, NomAutomatiqueMixin):
+    
+    str_format = "Point {self.lettre} ({self.x}, {self.y}, {self.z})"
+    
     def __init__(self, x, y, z):
         """
         initialisation d'un point dans l'espace
         """
+        super().__init__()
         self.x = x
         self.y = y
         self.z = z
-    
         
-    def __str__(self, other = None):
-        """
-        représentations d'un point dans l'espace
-        """
-        if other is None:
-            other = Point(self.x, self.y, self.z)
-        return "Point ({}, {}, {})\n".format(other.x, other.y, other.z)
-
     
     def __add__(self, other):
         """
@@ -83,7 +94,21 @@ class Point:
         dst = np.sqrt((pow(self.x - other.x,2),pow(self.y - other.y,2),pow(self.z - other.z,2)))
         
         return dst
+
+
+class Point2D(Point):
     
+    str_format = "Point2D {self.lettre} ({self.x}, {self.y})"
     
+    def __init__(self,x,y):
+        super().__init__(x,y,0)
+
+
+
+
 p = Point(1,2,3)
-p2 = Point(2,3,4)
+p += Point(2,3,4)
+print(p)
+p2d = Point2D(1,2)
+p2d += Point2D(3,0)
+print(p2d)
