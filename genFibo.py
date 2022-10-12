23c21abe22574d9de983357a77e8764028cdd976
MortimerAlien <113467961+MortimerAlien@users.noreply.github.com>
2022-10-12 22:43:17 +0200

    Update genFibo.py

--- genFibo.py
+++ genFibo.py
@@ -1,28 +1,26 @@
 #!/usr/bin/python3
-def genFibo(nbreUser:int) -> list:
+def genFibo(nbreUser:int) -> int:
+    #complexité temporelle de theta(n), pire des cas 0(n) (linéaire)
+    
     """
     Parameters
     ----------
     nbreUser : int
         nombre de la suite de fibo que l'utilisateur veut générer.
-
     Returns
     -------
     list
         la suite de nombre de fibo.
-
     """
-    suiteFibo = [0,1]
-    result = 1
-    sto = 1
-    cpt = 1
-    while nbreUser != 0:
-        sto = result
-        result = sto + suiteFibo[cpt - 1]
-        suiteFibo.append(result)
-        nbreUser -= 1
-        cpt+=1
-    return suiteFibo
+    
+    f0 = 0
+    f1 = 1
+    stock = []
+    for i in range(nbreUser):
+        f0 = f1
+        f1 = stock
+        stock = f0 + f1
+        print(stock)
 
 
 def demanderNombre()->int:
@@ -31,7 +29,6 @@ def demanderNombre()->int:
     -------
     int
         le nombre choisis par l'utilisateur.
-
     """
     nbreUser = input("merci de rentre un nombre: \n")
     try:
